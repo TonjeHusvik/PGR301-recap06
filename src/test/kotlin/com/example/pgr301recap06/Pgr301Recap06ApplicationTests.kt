b9a2325d187503cd5ee037e8c176d9930ae54c83
tonjehusvik <tonje.husvik@hotmail.com>
2022-10-11 08:50:51 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/pgr301recap06/Pgr301Recap06ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pgr301recap06
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Pgr301Recap06ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
